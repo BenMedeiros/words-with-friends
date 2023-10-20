cce7ffcda678d892bf0a8585ead542ea806271b6
Ben <ben.medeiros@planttherapy.com>
2023-10-20 15:17:24 -0600

    separated out tests into own folder

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ import {ButtonType} from "../html/tinyComponents/ButtonType.js";
 import {becomeSpyMaster} from "./app/spymaster.js";
 import {LabelInputType} from "../html/tinyComponents/LabelInputType.js";
 import api from "../server/local/api.js";
+import {runAllTests} from "../test/test_server_based_happy_path.js";
 
 const navBarEl = document.getElementById("navigation-bar");
 const mainEl = document.getElementById("main");
@@ -48,24 +49,4 @@ nameInput.onModified(()=>{
 
 
 
-
-api.newGame();
-api.updatePlayer('ben', 'red', 1);
-api.updatePlayer('sam', 'red', 2);
-api.updatePlayer('dave', 'blue', 3);
-api.updatePlayer('paul', 'blue', 4);
-
-api.startGame('food');
-
-api.submitClue(3, 'raw', 2);
-api.markGuesses(4, [8,2]);
-api.submitGuesses(4);
-
-api.submitClue(1, 'bar', 5);
-api.markGuesses(2, [1,4,9]);
-api.submitGuesses(2);
-
-
-api.submitClue(3, 'tool', 2);
-api.markGuesses(4, [13]);
-api.submitGuesses(4);
+runAllTests();
